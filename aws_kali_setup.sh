706f110f5bfc9a0fff1c54ce01ef44d23cdb23c2
Lezzor <45278922+Lezzor@users.noreply.github.com>
2024-01-19 10:41:42 +1100

    Update aws_kali_setup.sh

--- aws_kali_setup.sh
+++ aws_kali_setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
 sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
-sudo sed -i 's/allowed_users=.*/allowed_users=kali' /etc/X11/Xwrapper.config
+sudo sed -i 's/allowed_users=.*/allowed_users=kali/g' /etc/X11/Xwrapper.config
 sudo service xrdp start
 sudo systemctl enable xrdp
